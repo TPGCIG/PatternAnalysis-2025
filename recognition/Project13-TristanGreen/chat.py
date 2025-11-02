db252029cc875c9166dd45a7f0d53d565ce547e9
TPGCIG <tristan.green.300604@gmail.com>
2025-11-02 18:57:24 +1000

    added command line arg option to choose which weights you want to use.

--- recognition/Project13-TristanGreen/chat.py
+++ recognition/Project13-TristanGreen/chat.py
@@ -1,15 +1,21 @@
 import torch
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 from peft import PeftModel
+import argparse
+
+p = argparse.ArgumentParser()
+
+p.add_argument("--model_dir", required=True)
 
 # --- config ---
-ADAPTER_DIR = "runs/flan_t5_base_lora_biolaysumm_debug"
+ADAPTER_DIR = p.parse_args().model_dir
 BASE_MODEL = "google/flan-t5-base"
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-PREFIX = "summarize: "   # change to something else if you fine-tuned for a different task
+PREFIX = "summarize: "
 MAX_INPUT_LEN = 1024
 MAX_NEW_TOKENS = 256
 NUM_BEAMS = 4
+
 
 # --- load model ---
 print("Loading model...")
